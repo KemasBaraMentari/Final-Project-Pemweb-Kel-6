f1bb9e343d31f1d45ca4054d5f15f8957e166f43
KemasBaraMentari <baramentary@gmail.com>
2024-05-25 16:44:16 +0700

    halaman awal diubah

---
+++ tampilan/halaman-awal.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="../css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/custom/custom.css">
+
+  <title>Aroma Dapur</title>
+</head>
+
+<body>
+  <!-- As a heading -->
+  <div class="background">
+    <nav class="navbar navbar-light " style="background-color: #FFC994 !important;">
+      <div class="container-fluid">
+        <span data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" class="cursor-pointer"><img src="../assets/vectors/textalign-left.svg" alt="" width="40px"></span>
+        <span class="navbar-brand mb-0 h1"><img src="../Logo-AromaDapur.png" alt="" width="50px">AromaDapur</span>
+        <span class="navbar-brand mb-0 h1"><a href="halaman-pilihan.php" class="text-decoration-none text-dark"><img src="../assets/images/male_user.png" alt="" width="40px">Akun</a></span>
+      </div>
+    </nav>
+
+    <div class="container py-3">
+      <div class="d-flex justify-content-center">
+        <div class="col-md-4">
+          <input type="search" name="search" placeholder="Search.." class="search form-control rounded-pill border-0" autocomplete="off">
+        </div>
+      </div>
+
+      <div class="row py-5">
+        <div class="col-md-6 p-5">
+          <div class="card text-white" style="background-image: url('../assets/images/rectangle_105.jpeg'); height: 300px; background-size: cover; border-radius: 50px;">
+            <div class="card-body row align-items-end">
+              <!-- <img src="../assets/images/rectangle_105.jpeg" alt="" width="100%"> -->
+              <div class="row justify-content-center col-12">
+                <h3 class="text-center">Judull</h3>
+                <div class="d-flex justify-content-center">
+                  <a href="#" class="btn btn-outline-light rounded-pill px-5 border-4 fw-bold">Lihat</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 p-5">
+          <div class="card text-white" style="background-image: url('../assets/images/rectangle_105.jpeg'); height: 300px; background-size: cover; border-radius: 50px;">
+            <div class="card-body row align-items-end">
+              <!-- <img src="../assets/images/rectangle_105.jpeg" alt="" width="100%"> -->
+              <div class="row justify-content-center col-12">
+                <h3 class="text-center">Judull</h3>
+                <div class="d-flex justify-content-center">
+                  <a href="#" class="btn btn-outline-light rounded-pill px-5 border-4 fw-bold">Lihat</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 p-5">
+          <div class="card text-white" style="background-image: url('../assets/images/rectangle_105.jpeg'); height: 300px; background-size: cover; border-radius: 50px;">
+            <div class="card-body row align-items-end">
+              <!-- <img src="../assets/images/rectangle_105.jpeg" alt="" width="100%"> -->
+              <div class="row justify-content-center col-12">
+                <h3 class="text-center">Judull</h3>
+                <div class="d-flex justify-content-center">
+                  <a href="#" class="btn btn-outline-light rounded-pill px-5 border-4 fw-bold">Lihat</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 p-5">
+          <div class="card text-white" style="background-image: url('../assets/images/rectangle_105.jpeg'); height: 300px; background-size: cover; border-radius: 50px;">
+            <div class="card-body row align-items-end">
+              <!-- <img src="../assets/images/rectangle_105.jpeg" alt="" width="100%"> -->
+              <div class="row justify-content-center col-12">
+                <h3 class="text-center">Judull</h3>
+                <div class="d-flex justify-content-center">
+                  <a href="#" class="btn btn-outline-light rounded-pill px-5 border-4 fw-bold">Lihat</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    </div>
+    <!-- <img src="../assets/images/rectangle_105.jpeg" alt="" width="300px" height="200px"> -->
+
+
+    <!-- off canvas sidebar -->
+    <div class="offcanvas offcanvas-start text-white" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style="background-color: #531607">
+      <div class="offcanvas-header">
+        <span data-bs-dismiss="offcanvas" aria-label="Close" class="cursor-pointer"><img src="../assets/vectors/textalign-left2.svg" alt="" width="40px"></span>
+        <img src="../Logo-AromaDapur.png" alt="" width="70px">
+        <h5 class="offcanvas-title" id="offcanvasExampleLabel">AromaDapur</h5>
+      </div>
+      <div class="offcanvas-body">
+        <div>
+          Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+        </div>
+        <div class="dropdown mt-3">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+            Dropdown button
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- end off canvas sidebar -->
+
+
+  </div>
+
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="../js/bootstrap.js"></script>
+  <script src="../js/popper.min.js"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
