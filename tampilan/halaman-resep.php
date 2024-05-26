6b35f5dee6a5c36c84749c6997c1ff79e98398da
Fatan Izzatur <izzaturziyech22@gmail.com>
2024-05-26 18:15:10 +0700

    Add files via upload

---
+++ tampilan/halaman-resep.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="../css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/custom/custom.css">
+
+  <title>Detail Resep - Aroma Dapur</title>
+</head>
+
+<body>
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="../assets/images/gambar_makanan.jpg" alt="Gambar Makanan" class="img-fluid rounded">
+      </div>
+      <div class="col-md-6">
+        <h1 class="display-4">Nama Makanan</h1>
+        <p class="lead">Deskripsi singkat tentang makanan ini. Bisa berisi informasi mengenai asal-usul, rasa, dan keistimewaannya.</p>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-md-6">
+        <h3>Bahan-bahan</h3>
+        <ul>
+          <li>1 cup tepung terigu</li>
+          <li>2 butir telur</li>
+          <li>100 ml susu</li>
+          <li>Garam secukupnya</li>
+          <li>Minyak untuk menggoreng</li>
+          <!-- Tambahkan bahan-bahan lain sesuai resep -->
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <h3>Langkah Pembuatan</h3>
+        <ol>
+          <li>Campur tepung dan garam dalam wadah besar.</li>
+          <li>Tambahkan telur dan susu, aduk hingga merata.</li>
+          <li>Panaskan minyak dalam wajan.</li>
+          <li>Goreng adonan hingga matang dan berwarna keemasan.</li>
+          <li>Angkat dan sajikan.</li>
+          <!-- Tambahkan langkah-langkah lain sesuai resep -->
+        </ol>
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript; choose one of the two! -->
+  <script src="../js/bootstrap.js"></script>
+  <script src="../js/popper.min.js"></script>
+</body>
+
+</html>
