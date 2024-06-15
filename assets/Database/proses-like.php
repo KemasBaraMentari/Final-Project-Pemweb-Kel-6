4a9c9876e937aaf1c6725ac7056d01b42b2dabe7
Yantha <145485881+Yanthalk@users.noreply.github.com>
2024-06-15 23:41:44 +0700

    Update proses-like.php penambahan fungsi dislike

--- assets/Database/proses-like.php
+++ assets/Database/proses-like.php
@@ -7,10 +7,10 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
+require_once 'koneksi.php'; // Sesuaikan dengan path koneksi Anda
+
 // Memeriksa apakah request adalah POST dan mendapatkan recipe_id
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    require_once 'koneksi.php'; // Sesuaikan path koneksi dengan struktur aplikasi Anda
-
     // Memeriksa apakah recipe_id ada dan valid
     $recipe_id = isset($_POST['recipe_id']) ? intval($_POST['recipe_id']) : 0;
     
@@ -25,8 +25,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $result_check = $stmt_check->get_result();
         
         if ($result_check->num_rows > 0) {
-            // Pengguna sudah menyukai resep ini sebelumnya
-            echo "Anda sudah menyukai resep ini.";
+            // Pengguna sudah menyukai resep ini sebelumnya, lakukan dislike (hapus dari liked_recipes)
+            $delete_sql = "DELETE FROM liked_recipes WHERE user_id = ? AND recipe_id = ?";
+            $stmt_delete = $conn->prepare($delete_sql);
+            $stmt_delete->bind_param("ii", $user_id, $recipe_id);
+            
+            if ($stmt_delete->execute()) {
+                // Redirect kembali ke halaman resepku setelah dislike berhasil
+                if(isset($_SERVER['HTTP_REFERER'])) {
+                    header("Location: " . $_SERVER['HTTP_REFERER']);
+                } else {
+                    header("Location: ../../tampilan/halaman-awal.php"); // Redirect ke halaman resepku jika HTTP_REFERER tidak tersedia
+                }
+                exit();
+            } else {
+                echo "Error: " . $stmt_delete->error;
+            }
+            
+            $stmt_delete->close();
         } else {
             // Insert ke liked_recipes
             $insert_sql = "INSERT INTO liked_recipes (user_id, recipe_id) VALUES (?, ?)";
@@ -34,11 +50,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt_insert->bind_param("ii", $user_id, $recipe_id);
             
             if ($stmt_insert->execute()) {
-                // Redirect kembali ke halaman sebelumnya setelah like berhasil
+                // Redirect kembali ke halaman resepku setelah like berhasil
                 if(isset($_SERVER['HTTP_REFERER'])) {
                     header("Location: " . $_SERVER['HTTP_REFERER']);
                 } else {
-                    header("Location: ../../tampilan/halaman-awal.php"); // Redirect ke halaman default jika HTTP_REFERER tidak tersedia
+                    header("Location: ../../tampilan/halaman-awal.php"); // Redirect ke halaman resepku jika HTTP_REFERER tidak tersedia
                 }
                 exit();
             } else {
